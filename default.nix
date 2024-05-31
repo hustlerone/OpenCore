b24de4572b5c24bb572059619d2b88b1523a2fee
Hustler One <nine-ball@tutanota.com>
2024-05-31 16:32:28 +0200

    poopy reproducibility

---
+++ default.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.stdenv.mkDerivation {
+
+  name = "OpenCore";
+  src = ./src;
+
+  buildInputs = with pkgs; [
+    git
+    rustc
+    cargo
+  ]; # These are runtime deps
+
+  nativeBuildInputs = with pkgs; [
+
+  ]; # Dependencies only available in phases
+
+
+  buildPhase = ''
+    cargo build -r
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp target/release/$name $out/bin
+  '';
+
+
+}
